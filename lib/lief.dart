9f5584763bbe4fedb5031f876a0d30ef33914d3d
Liftu <louilou008@gmail.com>
2024-01-14 17:04:53 +0100

    Removed orignal placeholder code and added lief.dart and pe.dart

--- lib/lief.dart
+++ lib/lief.dart
@@ -1,8 +1,138 @@
-/// Support for doing something awesome.
+/// Dart bindings for LIEF.
 ///
-/// More dartdocs go here.
+///
 library;
 
-export 'src/lief_base.dart';
+import 'dart:io';
+import 'dart:ffi';
 
-// TODO: Export any libraries intended for clients of this package.
+import 'package:ffi/ffi.dart';
+import 'package:path/path.dart';
+
+import 'src/bindings/LIEF_bindings.dart';
+import 'src/pe.dart';
+
+export 'src/pe.dart';
+
+class Lief {
+  static final Lief _instance = Lief._internal();
+
+  factory Lief() {
+    return _instance;
+  }
+
+  static const String ARCH_X64 = "x64";
+  static const String ARCH_X86 = "x86";
+  static const String ARCH_AARCH64 = "aarch64";
+  static const String ARCH_ARM32 = "arm32";
+
+  static final String libsPath = join(Directory.current.path, "LIEF", "lib");
+  static final String linuxAarch64LibPath =
+      join(libsPath, "libLIEF_linux_${ARCH_AARCH64}.so");
+  static final String linuxX64LibPath =
+      join(libsPath, "libLIEF_linux_${ARCH_X64}.so");
+  static final String androidAarch64LibPath =
+      join(libsPath, "libLIEF_android_${ARCH_AARCH64}.so");
+  static final String androidArm32LibPath =
+      join(libsPath, "libLIEF_android_${ARCH_ARM32}.so");
+  static final String macosAarch64LibPath =
+      join(libsPath, "libLIEF_macos_${ARCH_AARCH64}.a");
+  static final String macosX64LibPath =
+      join(libsPath, "libLIEF_macos_${ARCH_X64}.a");
+  static final String iosAarch64LibPath =
+      join(libsPath, "libLIEF_ios_${ARCH_AARCH64}.a");
+  static final String winX64LibPath =
+      join(libsPath, "LIEF_win_${ARCH_X64}.dll");
+  static final String winX86LibPath =
+      join(libsPath, "LIEF_win_${ARCH_X86}.dll");
+
+  late DynamicLibrary dylib;
+
+  Lief._internal() {
+    final String architecture = getArchitecture();
+    String libraryPath = "";
+
+    if (Platform.isLinux) {
+      switch (architecture) {
+        case ARCH_X64:
+          libraryPath = linuxX64LibPath;
+          break;
+        case ARCH_AARCH64:
+          libraryPath = linuxAarch64LibPath;
+          break;
+      }
+    } else if (Platform.isAndroid) {
+      switch (architecture) {
+        case ARCH_AARCH64:
+          libraryPath = androidAarch64LibPath;
+          break;
+        case ARCH_ARM32:
+          libraryPath = androidArm32LibPath;
+          break;
+      }
+    } else if (Platform.isMacOS) {
+      switch (architecture) {
+        case ARCH_X64:
+          libraryPath = macosX64LibPath;
+          break;
+        case ARCH_AARCH64:
+          libraryPath = macosAarch64LibPath;
+          break;
+      }
+    } else if (Platform.isIOS) {
+      switch (architecture) {
+        case ARCH_X64:
+          libraryPath = iosAarch64LibPath;
+          break;
+      }
+    } else if (Platform.isWindows) {
+      switch (architecture) {
+        case ARCH_X64:
+          libraryPath = winX64LibPath;
+          break;
+        case ARCH_X86:
+          libraryPath = winX86LibPath;
+          break;
+      }
+    }
+
+    if (libraryPath.isEmpty) {
+      throw Exception("Unkown running platform (${Platform.operatingSystem})");
+    }
+    dylib = DynamicLibrary.open(libraryPath);
+  }
+
+  static String getArchitecture() {
+    String arch;
+    if (Platform.isWindows) {
+      arch = Platform.environment["PROCESSOR_ARCHITECTURE"]!;
+    } else {
+      final ProcessResult info = Process.runSync("uname", ["-m"]);
+      arch = info.stdout.toString().replaceAll("\n", "");
+    }
+    arch = arch.trim().toLowerCase();
+    switch (arch) {
+      case "x86_64" || "x64" || "amd64":
+        arch = ARCH_X64;
+        break;
+      case "x86" || "x32" || "i386" || "i686" || "386" || "amd32":
+        arch = ARCH_X86;
+        break;
+      case "aarch64" || "arm64" || "armv8b" || "armv8l":
+        arch = ARCH_AARCH64;
+        break;
+      case "arm" || "arm32" || "armv7l" || "armv6l":
+        arch = ARCH_ARM32;
+        break;
+      default:
+        throw Exception("Unknown architecture (${arch})");
+    }
+    return arch;
+  }
+
+  PeBinary parsePeFile(String filename) {
+    Pointer<Pe_Binary_t> pPeBinary =
+        LIEF(dylib).pe_parse(filename.toNativeUtf8().cast<Char>());
+    return PeBinary(peBinary: pPeBinary[0]);
+  }
+}
---
+++ lib/src/pe.dart
@@ -0,0 +1,279 @@
+import 'dart:ffi';
+
+import 'package:ffi/ffi.dart';
+import 'package:path/path.dart';
+
+import 'bindings/LIEF_bindings.dart';
+
+class PeBinary {
+  final Pe_Binary_t peBinary;
+  late String name;
+  late PeDosHeader dosHeader;
+  late PeHeader header;
+  late PeOptionalHeader optionalHeader;
+  late List<PeDataDirectory> dataDirectories;
+  late List<PeSection> sections;
+  late List<PeImport> imports;
+
+  PeBinary({required this.peBinary}) {
+    // name
+    name = basename(peBinary.name.cast<Utf8>().toDartString());
+
+    // dosHeader
+    dosHeader = PeDosHeader(dosHeader: peBinary.dos_header);
+
+    // ntHeader
+    header = PeHeader(header: peBinary.header);
+
+    // optionalHeader
+    optionalHeader = PeOptionalHeader(optionalHeader: peBinary.optional_header);
+
+    // dataDirectories
+    dataDirectories = <PeDataDirectory>[];
+    for (int i = 0; i < optionalHeader.numberofRvaAndSize; i++) {
+      dataDirectories
+          .add(PeDataDirectory(dataDirectory: peBinary.data_directories[i][0]));
+    }
+
+    // sections
+    sections = <PeSection>[];
+    for (int i = 0; i < header.numberOfSections; i++) {
+      sections.add(PeSection(peSection: peBinary.sections[i][0]));
+    }
+
+    // imports
+    imports = <PeImport>[];
+    for (int i = 0; i < (dataDirectories[1].size / 0x14 - 1); i++) {
+      imports.add(PeImport(peImport: peBinary.imports[i][0]));
+    }
+  }
+}
+
+class PeDosHeader {
+  final Pe_DosHeader_t dosHeader;
+  late int magic;
+  late int usedBytesInTheLastPage;
+  late int fileSizeInPages;
+  late int numberofRelocation;
+  late int headerSizeInParagraphs;
+  late int minimumExtraparagraphs;
+  late int maximumExtraparagraphs;
+  late int initialRelativeSs;
+  late int initialSp;
+  late int checksum;
+  late int initialIp;
+  late int initialRelativeCs;
+  late int addressofRelocationTable;
+  late int overlayNumber;
+  late List<int> reserved;
+  late int oemId;
+  late int oemInfo;
+  late List<int> reserved2;
+  late int addressofNewExeheader;
+
+  PeDosHeader({required this.dosHeader}) {
+    magic = dosHeader.magic;
+    usedBytesInTheLastPage = dosHeader.used_bytes_in_the_last_page;
+    fileSizeInPages = dosHeader.file_size_in_pages;
+    numberofRelocation = dosHeader.numberof_relocation;
+    headerSizeInParagraphs = dosHeader.header_size_in_paragraphs;
+    minimumExtraparagraphs = dosHeader.minimum_extra_paragraphs;
+    maximumExtraparagraphs = dosHeader.maximum_extra_paragraphs;
+    initialRelativeSs = dosHeader.initial_relative_ss;
+    initialSp = dosHeader.initial_sp;
+    checksum = dosHeader.checksum;
+    initialIp = dosHeader.initial_ip;
+    initialRelativeCs = dosHeader.initial_relative_cs;
+    addressofRelocationTable = dosHeader.addressof_relocation_table;
+    overlayNumber = dosHeader.overlay_number;
+    reserved = List<int>.filled(4, 0, growable: false);
+    for (int i = 0; i < 4; i++) {
+      reserved[i] = dosHeader.reserved[i];
+    }
+    oemId = dosHeader.oem_id;
+    oemInfo = dosHeader.oem_info;
+    reserved2 = List<int>.filled(10, 0, growable: false);
+    for (int i = 0; i < 10; i++) {
+      reserved2[i] = dosHeader.reserved2[i];
+    }
+    addressofNewExeheader = dosHeader.addressof_new_exeheader;
+  }
+}
+
+class PeHeader {
+  final Pe_Header_t header;
+  late int signature;
+  late int machine;
+  late int numberOfSections;
+  late int timeDateStamp;
+  late int pointertoSymbolTable;
+  late int numberofSymbols;
+  late int sizeofOptionalHeader;
+  late int characteristics;
+
+  PeHeader({required this.header}) {
+    signature = header.signature[0] |
+        header.signature[1] << 8 |
+        header.signature[2] << 16 |
+        header.signature[3] << 24;
+    machine = header.machine;
+    numberOfSections = header.numberof_sections;
+    timeDateStamp = header.time_date_stamp;
+    pointertoSymbolTable = header.pointerto_symbol_table;
+    numberofSymbols = header.numberof_symbols;
+    sizeofOptionalHeader = header.sizeof_optional_header;
+    characteristics = header.characteristics;
+  }
+}
+
+class PeOptionalHeader {
+  final Pe_OptionalHeader_t optionalHeader;
+  late int magic;
+  late int majorLinkerVersion;
+  late int minorLinkerVersion;
+  late int sizeofCode;
+  late int sizeofInitializedData;
+  late int sizeofUninitializedData;
+  late int addressofEntrypoint;
+  late int baseofData;
+  late int baseofCode;
+  late int imagebase;
+  late int sectionAlignment;
+  late int fileAlignment;
+  late int majorOperatingSystemVersion;
+  late int minorOperatingSystemVersion;
+  late int majorImageVersion;
+  late int minorImageVersion;
+  late int majorSubsystemVersion;
+  late int minorSubsystemVersion;
+  late int win32VersionValue;
+  late int sizeofImage;
+  late int sizeOFHeaders;
+  late int checksum;
+  late int subsystem;
+  late int dllCharacteristics;
+  late int sizeOfStackReverse;
+  late int sizeOfStackcommit;
+  late int sizeOfHeapReverse;
+  late int sizeOfHeapcommit;
+  late int loaderFlags;
+  late int numberofRvaAndSize;
+
+  PeOptionalHeader({required this.optionalHeader}) {
+    magic = optionalHeader.magic;
+    majorLinkerVersion = optionalHeader.major_linker_version;
+    minorLinkerVersion = optionalHeader.minor_linker_version;
+    sizeofCode = optionalHeader.sizeof_code;
+    sizeofInitializedData = optionalHeader.sizeof_initialized_data;
+    sizeofUninitializedData = optionalHeader.sizeof_uninitialized_data;
+    addressofEntrypoint = optionalHeader.addressof_entrypoint;
+    baseofData = optionalHeader.baseof_data;
+    baseofCode = optionalHeader.baseof_code;
+    imagebase = optionalHeader.imagebase;
+    sectionAlignment = optionalHeader.section_alignment;
+    fileAlignment = optionalHeader.file_alignment;
+    majorOperatingSystemVersion = optionalHeader.major_operating_system_version;
+    minorOperatingSystemVersion = optionalHeader.minor_operating_system_version;
+    majorImageVersion = optionalHeader.major_image_version;
+    minorImageVersion = optionalHeader.minor_image_version;
+    majorSubsystemVersion = optionalHeader.major_subsystem_version;
+    minorSubsystemVersion = optionalHeader.minor_subsystem_version;
+    win32VersionValue = optionalHeader.win32_version_value;
+    sizeofImage = optionalHeader.sizeof_image;
+    sizeOFHeaders = optionalHeader.sizeof_headers;
+    checksum = optionalHeader.checksum;
+    subsystem = optionalHeader.subsystem;
+    dllCharacteristics = optionalHeader.dll_characteristics;
+    sizeOfStackReverse = optionalHeader.sizeof_stack_reserve;
+    sizeOfStackcommit = optionalHeader.sizeof_stack_commit;
+    sizeOfHeapReverse = optionalHeader.sizeof_heap_reserve;
+    sizeOfHeapcommit = optionalHeader.sizeof_heap_commit;
+    loaderFlags = optionalHeader.loader_flags;
+    numberofRvaAndSize = optionalHeader.numberof_rva_and_size;
+  }
+}
+
+class PeDataDirectory {
+  final Pe_DataDirectory_t dataDirectory;
+  late int rva;
+  late int size;
+
+  PeDataDirectory({required this.dataDirectory}) {
+    rva = dataDirectory.rva;
+    size = dataDirectory.size;
+  }
+}
+
+class PeSection {
+  final Pe_Section_t peSection;
+  late String name;
+  late int virtualAddress;
+  late int size;
+  late int offset;
+  late int virtualSize;
+  late int pointertoRelocation;
+  late int pointertoLineNumbers;
+  late int characteristics;
+  late int contentSize;
+  // content
+  late double entropy;
+
+  PeSection({required this.peSection}) {
+    name = peSection.name.cast<Utf8>().toDartString();
+    virtualAddress = peSection.virtual_address;
+    size = peSection.size;
+    offset = peSection.offset;
+    virtualSize = peSection.virtual_size;
+    pointertoRelocation = peSection.pointerto_relocation;
+    pointertoLineNumbers = peSection.pointerto_line_numbers;
+    characteristics = peSection.characteristics;
+    // content
+    contentSize = peSection.content_size;
+    entropy = peSection.entropy;
+  }
+}
+
+class PeImport {
+  final Pe_Import_t peImport;
+  late String name;
+  late int forwarderChain;
+  late int timeDateStamp;
+  late List<PeImportEntry> entries;
+  late int importAddressTableRva;
+  late int importLookupTableRva;
+
+  PeImport({required this.peImport}) {
+    name = peImport.name.cast<Utf8>().toDartString();
+    forwarderChain = peImport.forwarder_chain;
+    timeDateStamp = peImport.timedatestamp;
+    entries = <PeImportEntry>[];
+    for (int i = 0; peImport.entries[i].address != 0; i++) {
+      entries.add(PeImportEntry(peImportEntry: peImport.entries[i][0]));
+    }
+    importAddressTableRva = peImport.import_address_table_rva;
+    importLookupTableRva = peImport.import_lookup_table_rva;
+  }
+}
+
+class PeImportEntry {
+  final Pe_ImportEntry_t peImportEntry;
+  late int isOrdinal;
+  late String name;
+  late int ordinal;
+  late int hintNameRva;
+  late int hint;
+  late int iatValue;
+  late int data;
+  late int iatAddress;
+
+  PeImportEntry({required this.peImportEntry}) {
+    isOrdinal = peImportEntry.is_ordinal;
+    name = peImportEntry.name.cast<Utf8>().toDartString();
+    ordinal = peImportEntry.ordinal;
+    hintNameRva = peImportEntry.hint_name_rva;
+    hint = peImportEntry.hint;
+    iatValue = peImportEntry.iat_value;
+    data = peImportEntry.data;
+    iatAddress = peImportEntry.iat_address;
+  }
+}
