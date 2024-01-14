f4f8ee8fb847e44ec900a794fc39b43554e4bd93
Liftu <louilou008@gmail.com>
2024-01-14 17:14:58 +0100

    Added tiny example for parsing PE binary

--- example/lief_example.dart
+++ example/lief_example.dart
@@ -1,3 +1,17 @@
 import 'package:lief/lief.dart';
 
-void main() {}
+void main() {
+  PeBinary binary = Lief().parsePeFile("LIEF/lib/LIEF_win_x86.dll");
+  print("PE file: ${binary.name}");
+  print("Magic: ${binary.dosHeader.magic.toRadixString(16).toUpperCase()}");
+
+  print("Sections:");
+  for (final PeSection section in binary.sections) {
+    print("\t${section.name}");
+  }
+
+  print("Imported libs:");
+  for (final PeImport import in binary.imports) {
+    print("\t${import.name}");
+  }
+}
